2148594b18ff4692ef8d4b9843e38ead6588a772
GaziRiad <ouhibimustapha36@gmail.com>
2026-03-17 17:42:45 +0100

    added patterns to cta

--- components/MainDiscount.tsx
+++ components/MainDiscount.tsx
@@ -10,8 +10,32 @@ const EventProduct = {
 
 export default function MainDiscount() {
   return (
-    <section className="bg-accent-400 relative mb-20 rounded-lg px-10 py-24">
-      <div className="max-w-4/6">
+    <section className="bg-accent-400 relative mb-20 overflow-hidden rounded-lg px-6 py-16 md:px-10 md:py-24">
+      {/* Diagonal Grid with Light */}
+      <div
+        className="pointer-events-none absolute inset-0 z-10 w-full"
+        style={{
+          backgroundImage: `
+          repeating-linear-gradient(45deg, rgba(0, 0, 0, 0.1) 0, rgba(0, 0, 0, 0.1) 1px, transparent 1px, transparent 20px),
+        repeating-linear-gradient(-45deg, rgba(0, 0, 0, 0.1) 0, rgba(0, 0, 0, 0.1) 1px, transparent 1px, transparent 20px)
+        `,
+          backgroundSize: "40px 40px",
+        }}
+      />
+
+      {/* Midnight Mist */}
+      <div
+        className="absolute inset-0 z-0"
+        style={{
+          backgroundImage: `
+          radial-gradient(circle at 50% 100%, rgba(70, 85, 110, 0.5) 0%, transparent 60%),
+          radial-gradient(circle at 50% 100%, rgba(99, 102, 241, 0.4) 0%, transparent 70%),
+          radial-gradient(circle at 50% 100%, rgba(181, 184, 208, 0.3) 0%, transparent 80%)
+        `,
+        }}
+      />
+
+      <div className="relative z-20 max-w-full md:max-w-4/6">
         <p className="text-accent-100">Don&apos;t Miss!!</p>
         <p className="text-primary mb-6 max-w-sm text-4xl font-semibold">
           Enhance Your Work Experience
@@ -27,7 +51,7 @@ export default function MainDiscount() {
         height={800}
         src={EventProduct.image}
         alt={`Image of ${EventProduct.title} from PRIMEPC.`}
-        className="absolute top-1/2 right-0 h-3/4 -translate-y-1/2 object-contain"
+        className="pointer-events-none absolute top-1/2 right-0 z-10 hidden h-3/4 -translate-y-1/2 object-contain md:block"
       />
     </section>
   );
